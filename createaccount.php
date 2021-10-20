f31af39f2443ce019a3616024bbbfee64844ad41
James Scheiber <jscheiber22@gmail.com>
2021-10-20 00:14:10 -0700

    now it adds to a database, which sounds like a fully inclusive LAMP stack if i've ever heard of one 8)

--- createaccount.php
+++ createaccount.php
@@ -1,13 +1,35 @@
 <?php
 
+ini_set('display_errors', 1);
+
+// Connect to local mysql server
+$servername = "localhost";
+$dbuser = "root";
+$dbpass = "root";
+$db = "accountDB";
+
+$conn = new PDO("mysql:host=$servername;dbname=$db", $dbuser, $dbpass);
+// if ($conn->connect_error){
+// 	die("Failed" . $conn->connect_error);
+// }
+
 if (isset($_POST['user'], $_POST['email'])){
 	$user = htmlspecialchars($_POST['user']);
 	$email = htmlspecialchars($_POST['email']);
 
-	echo "Thanks $user.</br>";
-	echo "You did good :)";
+	$sqlquery = "INSERT INTO users VALUES (\"$user\", \"$email\")";
+	$statement = $conn->prepare($sqlquery);
+	$statement->execute();
+
+	// if ($conn->query($sqlquery) === TRUE) {
+	// 	echo "Thanks $user.</br>";
+	// 	echo "You did good :)";
+	// } else {
+	// 	$error = "You did bad >:(";
+	// }
+
 } else {
-	echo "You did bad >:(";
+	$error = "You did bad >:(";
 }
 
 ?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <center>
+
+    <h1>Welcome :)</h1>
+    <br><br>
+
+    <h2><a href="createaccount.html">Create Account</a></h2>
+    <br>
+    <h2><a href="signin.html">Sign In</a></h2>
+
+  </center>
+</html>
